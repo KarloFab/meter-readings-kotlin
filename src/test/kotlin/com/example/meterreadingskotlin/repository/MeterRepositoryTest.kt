736eb7969da1167b97f71b1a2a7f23283bd33cfd
KarloFab <fabijanickarlo1@gmail.com>
2022-03-31 19:53:44 +0200

    Added repository test

---
+++ src/test/kotlin/com/example/meterreadingskotlin/repository/MeterRepositoryTest.kt
@@ -0,0 +1,31 @@
+package com.example.meterreadingskotlin.repository
+
+import com.example.meterreadingskotlin.domain.Meter
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+
+@DataJpaTest
+class MeterRepositoryTest {
+
+    @Autowired
+    lateinit var entityManager: TestEntityManager
+
+    @Autowired
+    lateinit var meterRepository: MeterRepository
+
+    @Test
+    fun whenFindAll_thenReturnMeters() {
+        val meter = Meter()
+        meter.name = "Test"
+
+        entityManager.persist(meter)
+        entityManager.flush()
+
+        val foundMeters = meterRepository.findAll()
+        assertThat(meter == foundMeters[0])
+    }
+
+}
